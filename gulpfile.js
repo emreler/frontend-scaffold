9d1255eb3761ebde6f5a6ab6f30b70b1df6cfb60
Emre Kayan <emrekayan@gmail.com>
2014-09-19 00:50:09 +0300

    added gulp to project, created 'default' and 'clean' tasks

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+$ = require('gulp-load-plugins')();
+
+gulp.task('default', ['clean'], function() {
+    return gulp.src('src/index.html')
+        .pipe($.usemin({
+            css: [$.minifyCss(), 'concat'],
+            js: [$.uglify(), 'concat']
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+// cleans "dist" folder which will be deployed to server
+gulp.task('clean', function() {
+    return gulp.src('public/*')
+        .pipe($.rimraf());
+});
