7d65278b53b09569817c5c341a97183a9cbaeae6
Emre Kayan <emrekayan@gmail.com>
2014-09-25 01:51:34 +0300

    updated gulp task to append revisioning to asset filenames

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
 var gulp = require('gulp');
 $ = require('gulp-load-plugins')();
 
-// minification, concatenation
-gulp.task('default', ['clean'], function() {
+// default task. concatenates, minifies/uglifies asset files, appends content hash to filename, replaces links in template file
+gulp.task('default', ['clean'], function () {
+
+    var jsFilter = $.filter("**/*.js");
+    var cssFilter = $.filter("**/*.css");
+    var userefAssets = $.useref.assets();
+
     return gulp.src('src/index.html')
-        .pipe($.usemin({
-            css: [$.minifyCss(), 'concat'],
-            js: [$.uglify(), 'concat']
-        }))
+        .pipe(userefAssets)
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.minifyCss())
+        .pipe(cssFilter.restore())
+        .pipe($.rev())
+        .pipe(userefAssets.restore())
+        .pipe($.useref())
+        .pipe($.revReplace())
         .pipe(gulp.dest('dist'));
 });
 
 // cleans "dist" folder which will be deployed to server
 gulp.task('clean', function() {
-    return gulp.src('public/*')
+    return gulp.src('dist/*')
         .pipe($.rimraf());
 });
 
